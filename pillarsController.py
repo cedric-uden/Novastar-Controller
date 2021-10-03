36f5382549f713bb1dddaf4e1c2a22997a8e8453
ICF Karlsruhe Multimedia <multimedia@icf-karlsruhe.de>
2021-10-03 14:39:35 +0200

    updated with version that was in use up to today which includes the basic fade functionality

--- pillarsController.py
+++ pillarsController.py
@@ -1,11 +1,27 @@
 import socket
 import sys
 import binascii
+import time
+import os
+
+
+# set cwnd to the files directory
+abs_path = os.path.abspath(__file__)
+dir_name = os.path.dirname(abs_path)
+os.chdir(dir_name)
+
 
 try:
     index = sys.argv[1]
 except:
     print("que passa")
+
+
+
+##### SET THE TARGET FADE TIME
+FADE_TIME = 2
+##### SET THE TARGET FADE TIME
+
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,33 +30,57 @@ server_address = ('192.168.0.78', 5200)
 # Connect to socket
 sock.connect(server_address)
 
-# The XX byte codes the brightness value (0-255) in HEX and T codes left pillar (4) and right pillar (5). 
-# The other byte changes are not known to us ...
-#                                  T                    XX
-left_panel_off =   "55aa00c5fe000104ffff0100010000020100002059"
-right_panel_off =  "55aa00c6fe000105ffff0100010000020100002259"
 
-left_20_percent =  "55aa00dcfe000104ffff0100010000020100366d59"
-right_20_percent = "55aa00ddfe000105ffff0100010000020100366f59"
+def main():
+    try:
+        if index == "off":
+            fade_out()
+        elif index == "on":
+            fade_in()
+        elif index == "test":
+            sock.sendall(bytearray.fromhex(testing_hex))
+    finally:
+        sock.close()
 
-left_30_percent =  "55aa0081fe000104ffff01000100000201004c2859"
-right_30_percent = "55aa0082fe000105ffff01000100000201004c2a59"
 
 
-try:
-    
-    # Send data
-    if index == "off":
-        sock.sendall(bytearray.fromhex(left_panel_off))
-        sock.sendall(bytearray.fromhex(right_panel_off))
+# read a file and  return the content
+def readfile(file):
+    with open(file) as f:
+        return f.readlines()
+    f.close
 
-    elif index == "on":
-        sock.sendall(bytearray.fromhex(left_30_percent))
-        sock.sendall(bytearray.fromhex(right_30_percent))
 
-    elif index == "test":
-        sock.sendall(bytearray.fromhex(testing_hex))
 
-finally:
-    # Close socket
-    sock.close()
+# read the files
+all_left_panel_values = readfile("fader/left-pillar-fade-values.csv")
+all_right_panel_values = readfile("fader/right-pillar-fade-values.csv")
+
+# set the counter of available steps to fade to and determine the fade interval
+step_counter = len(all_left_panel_values)
+# remove 30 percent to compensate other overhead delay
+fade_interval = (FADE_TIME / step_counter) * 0.7
+
+
+# copy pasta simple array access loops for both tables
+
+def fade_in():
+    for i in range (0, step_counter - 1):
+        line = all_left_panel_values[i][:-1]
+        sock.sendall(bytearray.fromhex(line))
+        line = all_right_panel_values[i][:-1]
+        sock.sendall(bytearray.fromhex(line))
+        time.sleep(fade_interval)
+
+
+def fade_out():
+    for i in range (step_counter - 2, -1, -1):
+        line = all_left_panel_values[i][:-1]
+        sock.sendall(bytearray.fromhex(line))
+        line = all_right_panel_values[i][:-1]
+        sock.sendall(bytearray.fromhex(line))
+        time.sleep(fade_interval)
+
+
+# call the main function
+main()
